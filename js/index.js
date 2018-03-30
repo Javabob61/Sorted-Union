906bd2bef30597e539ed4c5751eed50a5bcee4b0
Robert Van Housen <33345296+Javabob61@users.noreply.github.com>
2018-03-29 23:14:31 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,28 +5,32 @@ function uniteUnique(arr) {
                                      // otherwise, only the first Array will be accessable.
     var concatArray = args[0];      // sets the beginning value of concatArray.  
     var finalArray = [];
+    var tempArray = [];
  
     for (var i = 1; i < args.length; i++) {
       concatArray = concatArray.concat(args[i]);      
     }
   
-    finalArray = concatArray;
+    finalArray = concatArray;    
     
-    console.log(concatArray);
     var arrayLength = concatArray.length;      
   
-    for (var j = 0; j < arrayLength; j++) {      
-      //console.log("j = " + j);       
-      for (var k = j + 1; k < arrayLength; k++) {
-        //console.log("k = " + k);
+    for (var j = 0; j < arrayLength; j++) { 
+      for (var k = j + 1; k < arrayLength; k++) {       
           if (finalArray[j] === finalArray[k]) {
-          finalArray = finalArray.splice(0, k);
-          console.log(finalArray);
-        }     
+          finalArray[k] = "";
+          }     
       }       
-    }      
-   //console.log(finalArray);  
-  //return finalArray;
+    }
+  
+    for (var l = 0; l < arrayLength; l++) {        
+        if (finalArray[l] !== "") {
+          tempArray.push(finalArray[l]);
+        }     
+    } 
+  
+    console.log(tempArray);  
+    return tempArray;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
